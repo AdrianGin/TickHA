c0f9f5bd08304a2fe5595462eac5ed9404075326
Tick <mjr.tick@gmail.com>
2016-05-29 18:49:54 +1200

    Added loader post build

--- HomeAutomation/WirelessTransmitter/Arduino/postbuild_arduino.bat
+++ HomeAutomation/WirelessTransmitter/Arduino/postbuild_arduino.bat
@@ -1,7 +1,8 @@
 set filename=%1
+set comport=%2
 
 avr-objcopy -j .text -j .data %filename%.elf -Obinary %filename%.bin
 avr-objcopy -j .text -j .data %filename%.elf -Oihex %filename%.hex
 avr-size %filename%.elf
 avr-objdump -h -S %filename%.elf > %filename%.lst
-avrdude -c arduino -Pcom8 -b57600 -pm328p -e -U flash:w:%filename%.hex:i
+avrdude -c arduino -P%comport% -b57600 -pm328p -e -U flash:w:%filename%.hex:i
