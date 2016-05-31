84541031fa29f21f7930fef424b90f6c338bb592
Tick <mjr.tick@gmail.com>
2016-06-01 11:51:39 +1200

    Restructuring files

--- HomeAutomation/WirelessReceiver/Release/makedep.mk
+++ HomeAutomation/WirelessReceiver/Release/makedep.mk
@@ -2,9 +2,7 @@
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
+..\SharedLib\Devices\nRF24L01.c
+
 main.c
-
-nrf24.c
-
-radioPinFunctions.c
 
